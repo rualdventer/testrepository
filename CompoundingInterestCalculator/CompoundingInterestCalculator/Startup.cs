fcd035159e957e1218ca143c67040cd6ea408998
rualdventer <rualdventer@gmail.com>
2014-03-20 09:54:34 +0200

    Added CompoundingInterestCalculator project

---
+++ CompoundingInterestCalculator/CompoundingInterestCalculator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CompoundingInterestCalculator.Startup))]
+namespace CompoundingInterestCalculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
