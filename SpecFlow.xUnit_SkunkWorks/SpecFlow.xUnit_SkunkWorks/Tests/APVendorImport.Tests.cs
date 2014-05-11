022aecc841686db6abba4174b2b0a37799aed36d
rualdventer <rualdventer@gmail.com>
2014-05-11 19:25:58 +0200

    Added SpecFlow.xUnit R&D Project

---
+++ SpecFlow.xUnit_SkunkWorks/SpecFlow.xUnit_SkunkWorks/Tests/APVendorImport.Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace SpecFlow.xUnit_SkunkWorks
+{
+    //[TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void ApVendorImport()
+        {
+
+        }
+    }
+}
